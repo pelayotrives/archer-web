c9f6a7e7cff6e668d12a0e0a874c7afc3082c048
Pelayo Trives <pelayotrives@gmail.com>
2023-12-29 02:18:23 +0100

    Feat: ID folder created.

---
+++ src/app/(main)/projects/[id]/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Head from "next/head";
+
+export default function Projects() {
+  return (
+    <>
+      <Head>
+        <title>Archer Arquitectura | Proyecto</title>
+        <meta property="og:title" content="Archer Arquitectura | Proyecto" key="title"/>
+        <meta name="description" content="Descubre todos los proyectos en los que ha participado a lo largo de su trayectoria Archer Arquitectura, estudio de arquitectura ubicado en Madrid."/>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className="bg-primary_bg">
+        <section className="mx-sp8 min-h-[calc(100vh-80px)] h-full">
+            <p>Test Proyecto ID</p>
+        </section>
+      </main>
+    </>
+  );
+}
