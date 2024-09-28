2b5de8551158b06c1f030c5d1c96771a568c6839
Pelayo Trives <pelayotrives@gmail.com>
2024-09-28 19:52:32 +0200

    fix: updated broken tailwind.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,8 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     fontFamily: {
